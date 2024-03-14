f3eefb76bba37b527f56862f3101e9cade00de61
fashionman <tema5286@gmail.com>
2024-03-14 04:53:52 +0300

    feat: Added base service

---
+++ TestTask/Services/Implementations/ServiceBase.cs
@@ -0,0 +1,18 @@
+﻿using TestTask.Data;
+
+namespace TestTask.Services.Implementations
+{
+    public abstract class ServiceBase : IDisposable
+    {
+        private protected ApplicationDbContext _db;
+        public ServiceBase(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Dispose()
+        {
+            ((IDisposable)_db).Dispose();
+        }
+    }
+}
