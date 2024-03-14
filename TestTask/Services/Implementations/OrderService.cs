8e40ab1fa787421e9b81900b8c3a209505d0e643
fashionman <tema5286@gmail.com>
2024-03-14 05:06:49 +0300

    feat: Added function GetOrder in OrderService

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : ServiceBase, IOrderService
+    {
+        public OrderService(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public Task<Order> GetOrder()
+        {
+            return _db.Orders
+                .Select(o => new { Order = o, Amount = o.Price * o.Quantity })
+                .OrderByDescending(o => o.Amount)
+                .Select(o => o.Order)
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
