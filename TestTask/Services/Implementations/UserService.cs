d06474efbcf40e290d2da8c8b254b71c7cbeeb03
fashionman <tema5286@gmail.com>
2024-03-14 04:59:51 +0300

    feat: Added function GetUser in UserService

---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : ServiceBase, IUserService
+    {
+        public UserService(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public Task<User> GetUser()
+        {
+            return _db.Users
+                .Select(u=> new { User = u, CountOrders = u.Orders.Count })
+                .OrderByDescending(u => u.CountOrders)
+                .Select(u=>u.User)
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
